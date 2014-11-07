a43641012e63e71ae8930a543bde27ed17bb83bf
ismtabo <ismaeljose.taboada@alumnos.uva.es>
2014-11-07 11:00:50 +0100

    Cambios en:  ·test.py:   -getter de la informacion del test   -setter de nueva pregunta en el test  ·pregunta.py:   -cambio de getRespuestaCorrecta a getTextRespuestaCorrecta (obtiene respuesta correcta en formato texto)   -getter de respuesta correcta (no formato texto)  ·writefile.py:   -cambiado el getContent a setContent, metodo para escribir test en fichero

--- Creator/writefile.py
+++ Creator/writefile.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python
-#  
+#
 # -*- coding: utf-8 -*-
 
 
 """
 	Interfaz de la entrada y salida del programa.
 """
+
+
 class Write:
+    def __init__(self, path, rwa='r', coding='utf-8'):
+        if path.split('/')[-1].split('.')[-1] == 'csv':
+            self.path = path
+            self.rwa = rwa
+            self.format = format
+        else:
+            self.path = ''
+            self.rwa = ''
+            raise IOError('File path is not a csv file')
 
 
-	def __init__(self,path,rwa='r',coding='utf-8'):
-		if path.split('/')[-1].split('.')[-1]=='csv':
-			self.path = path
-			self.rwa = rwa
-			self.format = format
-		else:
-			raise IOError('File path is not a csv file')
+    def setContent(self, test):
+        testfile = open(self.path, self.rwa)
+        infor = ';;'.join(test.getInfo())
+        testfile.write(infor)
+        for key in test.getQuestions().keys():
+            with test.getQuestions()[key] as question:
+                testfile.write("""{};;{};;{};;{}""".format(question.getPregunta(), question.getRespuestaCorrecta(),
+                               question.getMultiple(),
+                               ';;'.join([')'.join(question[key]) for key in question.getRespuestas().keys()])))
+        testfile.close()
 
-	def getContent(self):
-		with open(self.path, self.rwa) as testfile:
-			lines = testfile.readlines()
-		lines = [line.rstrip('\n') for line in lines]
-		infor = lines[0].split(';;')
-		questions = map(lambda x: x.split(';;'),lines[1:])
-		testfile.close()
-		return infor,questions
 
--- Reader/tests.py
+++ Reader/tests.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-#  
+#!/usr/bcontentn/env python
+#
 # -*- coding: utf-8 -*-
 
 from random import shuffle
@@ -14,94 +14,104 @@ NAME = 'name'
 NQUESTIONS = 'nq'
 
 msg = {RIGHT: "Respuesta correcta", WRONG: "Respuesta incorrecta", EMPTY: "Respuesta sin contestar"}
-marks = {9.0:"Matricula", 8.0: "Sobresaliente", 7.0:"Notable", 6.0:"Aprobado", 5.0:"Suficiente", -1.0:"Suspenso"}
+marks = {9.0: "Matricula", 8.0: "Sobresaliente", 7.0: "Notable", 6.0: "Aprobado", 5.0: "Suficiente", -1.0: "Suspenso"}
 info_cabecera = ['name', 'nq', RIGHT, WRONG, EMPTY]
 
+
 class Test:
-	"""
+    """
 		Implementacion de un Test con preguntas de seleccion unica:
 			Atributos:
 				info = diccionario de la informacion del Test
 				preguntas = list<Preguntas> 
 	"""
 
-	def __init__(self,info,content):
-		"""
+    def __init__(self, info, content):
+        """
 		Inicializa una instancia de Test
 		:param info: informacion del test
 		:param content: lista de preguntas del test
 		"""
-		info = [info[i] if not i > 0 else ((int)(info[i]) if not i > 1 else (float)(info[i])) for i in range(len(info))]
-		self.info = dict(zip(info_cabecera,info))
-		self.content = [Pregunta(i[0],i[1],i[3:],i[2]) for i in content]
-		shuffle(self.content)
-		self.content = dict(zip(range(1,len(self.content)+1),self.content))
-		self.counter = {RIGHT:0,WRONG:0,EMPTY:0}
-		
-	
-	def getQuestions(self,key):
-		"""
+        info = [info[i] if not i > 0 else ((int)(info[i]) if not i > 1 else (float)(info[i])) for i in range(len(info))]
+        self.info = dict(zip(info_cabecera, info))
+        self.content = [Pregunta(i[0], i[1], i[3:], i[2]) for i in content]
+        shuffle(self.content)
+        self.content = dict(zip(range(1, len(self.content) + 1), self.content))
+        self.counter = {RIGHT: 0, WRONG: 0, EMPTY: 0}
+
+
+    def getQuestions(self, key):
+        """
 		:param key: - indice de la pregunta
 		:return: pregunta correspondiente al indice
 		"""
-		return self.content[key]
+        return self.content[key]
 
-	def getNumQuestions(self):
+    def getNumQuestions(self):
+        """
+		:return: numero de las preguntas del test
 		"""
-		:return: informacion del test
-		"""
-		return self.info[NQUESTIONS]
-	
-	def getQResult(self,key,answer):
-		"""
+        return self.info[NQUESTIONS]
+
+    def getInfo(self):
+        """
+        :return: informacion del test
+        """
+        return self.info
+
+    def getQResult(self, key, answer):
+        """
 		Dada la respuesta del usuario y la id de la pregunta incrementa el correspondiente
 		:param key: indice de la pregunta correspondiente
 		:param answer: respuesta del usuario
 		"""
-		result = 0
-		if(answer==str(0)):
-			result = EMPTY
-		elif (self.content[key].correcta(answer)):
-			result = RIGHT
-		else:
-			result = WRONG
-		self.counter[result] += 1
-		print 'result:',result
-		return msg[result],':',(self.content[key].getRespuestaCorrecta() if result==WRONG else '')
-	
-	def finalResult(self):
-		"""
+        result = 0
+        if (answer == str(0)):
+            result = EMPTY
+        elif (self.content[key].correcta(answer)):
+            result = RIGHT
+        else:
+            result = WRONG
+        self.counter[result] += 1
+        print 'result:', result
+        return msg[result], ':', (self.content[key].getTextRespuestaCorrecta() if result == WRONG else '')
+
+    def finalResult(self):
+        """
 		:return: valor de contadores, nota final y calificacion
 		"""
-		return """--------------------------------------------------------
+        return """--------------------------------------------------------
 Respuestas correctas: 		{}
 Respuestas incorrectas: 	{}
 Respuestas sin contestar: 	{}
 Nota final:			{}
 Resultado:			{}
-		""".format(self.counter[RIGHT],self.counter[WRONG],self.counter[EMPTY],self.nummark(),self.abcmark(self.nummark()))
+		""".format(self.counter[RIGHT], self.counter[WRONG], self.counter[EMPTY], self.nummark(),
+                   self.abcmark(self.nummark()))
 
-	def nummark(self):
-		"""
+    def nummark(self):
+        """
 		:return: nota numerica del examen
 		"""
-		return (self.counter[RIGHT]*self.info[RIGHT]-self.counter[WRONG]*self.info[WRONG]-self.counter[EMPTY]*self.info[EMPTY])//self.getNumQuestions()*10
-		
-	def abcmark(self,nmark):
-		"""
+        return (self.counter[RIGHT] * self.info[RIGHT] - self.counter[WRONG] * self.info[WRONG] - self.counter[EMPTY] *
+                self.info[EMPTY]) // self.getNumQuestions() * 10
+
+    def abcmark(self, nmark):
+        """
 		:return: calificacion del examen
 		"""
-		for key in marks.keys():
-			if nmark > key:
-				return marks[key] 
-		else:
+        for key in marks.keys():
+            if nmark > key:
+                return marks[key]
+        else:
             return marks[-1]
 
-    def setNewQuestion(self,question):
+
+    def setNewQuestion(self, question):
         """
-        :param question:
-        :return:
+        :param question: nueva del test
         """
+        self.content[self.content.keys()[-1]] = Pregunta(question[0], question[1], question[3:], question[2])
         pass
 	
 
