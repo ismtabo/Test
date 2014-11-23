236f3055a30c542f65a20fceeefd80b29d1cfd63
ismtabo <ismaeljose.taboada@alumnos.uva.es>
2014-11-23 15:28:05 +0100

    Corregido  manejador de la vista: -Conectados eventos

--- Gui/view.py
+++ Gui/view.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 # 
 # -*- coding: utf-8 -*-
+import logging
+import os
 
 import sys
 try:
     import pygtk
     pygtk.require("2.0")
+    print 'pygtk importado'
 except:
     pass
 try:
     import gtk
     import gtk.glade
+    print 'gtk importado'
 except:
     sys.exit(1)
 
@@ -31,29 +35,42 @@ class GTKWindow:
         self.point_correct = self.builder.get_object("correct_spin")
         self.point_wrong = self.builder.get_object("wrong_spin")
         self.point_na = self.builder.get_object("na_spin")
+        print 'variables de la vista acopladas'
 
         # Enlazar eventos con los manejadores de eventos
         # TODO enlazar eventos con funciones del programa
         eventos = {
         "on_view_delete_event": gtk.main_quit,
-        "on_new_button_press_event" : self.none ,
-        "on_open_button_press_event" : self.none ,
-        "on_save_button_press_event" : self.none ,
-        "on_save_as_button_press_event" : self.none ,
-        "on_quit_button_press_event" : self.none ,
-        "on_quest_add_button_press_event" : self.none ,
-        "on_quest_rm_button_press_event" : self.none ,
+        "on_new_activate" : self.none ,
+        "on_open_activate" : self.none ,
+        "on_save_activate" : self.none ,
+        "on_save_as_activate" : self.none ,
+        "on_quit_activate" : gtk.main_quit ,
+        "on_quit_select" : gtk.main_quit ,
+        "on_quest_add_clicked" : self.none ,
+        "on_quest_rm_clicked" : self.none ,
         "on_quests_list_select_cursor_row" : self.none ,
-        "on_answer_add_button_press_event" : self.none ,
-        "on_quest_r_button_press_event" : self.none
+        "on_answer_add_clicked" : self.none ,
+        "on_answer_rm_clicked" : self.none
         }
         self.builder.connect_signals(eventos)
-        gtk.main()
+
+        self.num_questions.set_text(str(0))
+        self.correct_answers.set_text("")
+
+        logging.info('eventos conectados')
+
         
-    def none(self):
-        print "event"
+    def none(self,widget,data=None):
+        os.system("notify-send \"Evento en\" \"{}\"".format(widget))
         pass
+
+    def main(self):
+        print 'ejecutando main() '
+        gtk.main()
+        print "fin"
+
 
 if __name__ == "__main__":
     window = GTKWindow()
-    gtk.main()
+    window.main()
